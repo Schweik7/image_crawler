565f397acfd8b98ff32bb69cd3d868ed85563d7a
Schweik7 <weibidewo0@gmail.com>
2023-09-17 16:23:51 +0800

    0917

--- image_crawler.py
+++ image_crawler.py
@@ -13,19 +13,15 @@ from DrissionPage.errors import ElementNotFoundError
 from loguru import logger
 
 # Flickr
-# Unsplash
-# Pixabay
 # Pexels
 # Pinterest
 pexels_url = "https://www.pexels.com/search/{}"
 pexels_url_2 = "https://www.pexels.com/zh-cn/search/{}"
-pexels_img = "https://images.pexels.com/photos/1120049/pexels-photo-1120049.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
 
-unsplash_url = "https://unsplash.com/s/photos/{}"
-unsplash_img = "https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2F0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
-
-pixabay_url = "https://pixabay.com/images/search/{}"
-pixabay_img = "https://cdn.pixabay.com/photo/2017/08/07/18/57/dog-2606759__340.jpg"
+website_urls={
+    "unsplash":"https://unsplash.com/s/photos/{}",
+    "pixabay" : "https://pixabay.com/images/search/{}"
+}
 
 proxies = {
     "http": "http://127.0.0.1:10809",
@@ -73,7 +69,7 @@ class Image_crawler:
             set_paths(user_data_path="./user_data")
         self.page = WebPage()
         if recoder == "csv":
-            self.recorder = Recorder("imager_src_info.csv", cache_size=10)
+            self.recorder = Recorder("imager_src_info.csv", cache_size=20)
             self.recorder._encoding = "utf-8-sig"
         else:
             self.recorder = DBRecorder(
@@ -91,11 +87,7 @@ class Image_crawler:
             os.makedirs(picpath)
 
     def unsplash_crawl(self, keywords: str = "cat", image_cnt: int = 200) -> bool:
-        self.cur_image_cnt = 0
-        self.page.get(unsplash_url.format(keywords))
-        # self.page.download_set.by_browser()
-        self.page.download_set.by_DownloadKit()  # 是通过requests进行下载的
-        logger.info(f"[+] 开始搜索以{keywords}为关键词的图片")
+        self.page_init(keywords,website="unsplash")
         if button := self.page("Load more", timeout=3):
             button.click(by_js=True, timeout=2.5)  # 先按load more
             sleep(2.5)
@@ -112,47 +104,73 @@ class Image_crawler:
             # img[data-test=photo-grid-masonry-img]可获取图片元素
             if len(self.page.s_eles("css:figure[itemprop=image]")) > image_cnt:
                 for fig_ele in self.page.eles("css:figure[itemprop=image]"):
-                    image = Image()
-                    image.key_words = keywords
-                    img_ele: ChromiumElement = fig_ele.ele(
+                    self.unsplash_image_extract(keywords, fig_ele)
+                self.recorder.record()
+        return True
+
+    def page_init(self, keywords,website="unsplash"):
+        self.cur_image_cnt = 0
+        self.page.get(website_urls[website].format(keywords)) # 不同网站用不用的搜索网址
+        # self.page.download_set.by_browser()
+        self.page.download_set.by_DownloadKit()  # 是通过requests进行下载的
+        logger.info(f"[+] 开始搜索以{keywords}为关键词的图片")
+
+    def unsplash_image_extract(self, keywords, fig_ele):
+        image = Image()
+        image.key_words = keywords
+        img_ele: ChromiumElement = fig_ele.ele(
                         "css:img[data-test=photo-grid-masonry-img]"
                     )
-                    image.title = img_ele.attr("alt")
-                    image.tag = (
+        image.title = img_ele.attr("alt")
+        image.tag = (
                         fig_ele.child().child(index=2).raw_text.replace("\n", ";")
                     )
-                    filename = (
+        filename = (
                         f"images/{keywords}/{keywords}_{self.cur_image_cnt}_"
                         f'{datetime.now().strftime("%Y%m%d-%H%M")}'
                     )
-                    if not self.to_jpg:
-                        image.location = filename + ".avif"
-                    else:
-                        image.location = filename + ".jpg"
-                    path = os.path.dirname(image.location)
-                    rename = os.path.basename(image.location)
+        if not self.to_jpg:
+            image.location = filename + ".avif"
+        else:
+            image.location = filename + ".jpg"
+        path = os.path.dirname(image.location)
+        rename = os.path.basename(image.location)
                     # image.link=img_ele.prop("currentSrc")#会返回一个plus.unsplash.com
-                    image.link = fig_ele("css:a[itemprop=contentUrl]").link
+        image.link = fig_ele("css:a[itemprop=contentUrl]").link
                     # save方法中Path(path).mkdir(parents=True, exist_ok=True)
-                    if not img_ele.prop("currentSrc"):
-                        logger.info(f"[x] 标题为{image.title}的图片将单独下载")
+        if not img_ele.prop("currentSrc"):
+            logger.info(f"[x] 标题为{image.title}的图片将单独下载")
                         # self.page.download.add(file_url=img_ele.attr("src"),goal_path=path,rename=rename) # 多线程的下载西似乎有些问题
-                        self.page.download(
+            self.page.download(
                             file_url=img_ele.attr("src"),
                             goal_path=path,
                             rename=rename,
                             file_exists="overwrite",
-                            show_msg=True,
                             headers=fake_headers,
                             proxies=proxies,
                         )
-                    else:
+        else:
                         # save函数主要是通过currentSrc存储的
-                        img_ele.save(path=path, rename=rename)
-                    logger.debug(image)
-                    self.recorder.add_data(asdict(image))
-                    self.cur_image_cnt += 1
-                self.recorder.record()
+            img_ele.save(path=path, rename=rename)
+        logger.debug(image)
+        self.recorder.add_data(asdict(image))
+        self.cur_image_cnt += 1
+
+
+    def pixabay_crawl(self, keywords: str = "cat", image_cnt: int = 200) -> bool:
+        self.page_init(keywords=keywords,website="pixabay")
+        while self.cur_image_cnt<image_cnt:
+            self.page.scroll.down(800)
+            sleep(0.2)
+            self.page.scroll.up(200)
+            sleep(0.2)
+            if len(figs:=self.page.eles('css:div[class^="column"] img')) > image_cnt:
+                for fig in figs:
+                    pass
+        def pixabay_image_extract(self, keywords, fig_ele):
+            image=Image()
+            image.key_words=keywords
+            image.
 
 
 if __name__ == "__main__":

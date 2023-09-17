f0483056010643e23829f496b43416cc396895a7
Meng-07 <13550928106@139.com>
2023-09-17 22:45:37 +0800

    add restoration

---
+++ restoration.py
@@ -0,0 +1,173 @@
+#中文关键词0917版
+农田、农场、种植园、果园、林地、狩猎地、畜牧场、草地、养殖场、渔场、鱼塘、温室大棚、谷仓
+市场、菜站、果蔬店、肉类店、超市、便利店
+厨房、烘焙坊、食品工厂、食物储藏室
+餐厅、食堂、餐馆、快餐店、酒吧、宴会厅、咖啡厅、野餐地、餐车、食品展览地
+住宅、公寓、小区、别墅、农村、城中村、养老院宿舍、家庭、房屋、卧室、部落聚居地
+旅馆/酒店、民宿/客栈、青年旅社、农家乐、度假村、出租屋
+篷车、房车、帐篷、蒙古包、船屋
+供水公司、电力公司、天然气供应点
+社区中心和活动中心、广场、体育和健身设施、公共绿地和休闲设施
+公交车、公路/道路、桥梁、隧道
+火车、高铁、列车、动车、地铁、轻轨、空轨、有轨电车、磁悬浮
+机场、飞机
+港口、码头、船只、快艇、游艇、舰艇、独木舟、木筏、竹筏、橹船、龙舟、羊皮筏子
+自行车道、人行道、步行街区、公园道、健身步道、小径
+公安、治安管理、城管、警务站点、警车、法院、检察院、律所、银行
+消防站、急救车、防空洞、避难所、地道、救助站
+交通信号灯、标志设施（需要带上环境）、交警岗亭、交通监控
+门卫、社区监控、门禁、巡逻车、安保
+医院、卫生院、健康中心、康复中心、中医馆、水疗中心、疗养院、诊所和医务室、药房和药店、疾病预防控制中心、疫苗接种点、献血车/站、救护车、
+学校/校园、教学楼、教室、课堂、实验室、研讨会、学习空间（读书角）等
+操场（运动场）、图书馆、体育馆、自习室、咖啡厅、多功能厅（礼堂）、学生活动中心、社团活动、校园景观
+书房、家长监督作业、亲子教学、绘本阅读、户外探索、做家务、做饭等日常生活场景
+家庭会议、家庭聚餐、节日庆祝、家庭出游、亲子游戏、生日派对、电影之夜、家庭出游等
+社区学习中心、教育机构（培训）、社区课程、公共图书馆、志愿者教学、夜校、社区学院等、老年大学
+科技馆、博物馆、艺术展、宣传栏、书店、街道活动、社区志愿者活动、社区讲座、社区会议、邻里联谊会、文体活动、社区乐队演出、园游会等
+网络课程、在线教师、学习资源、在线学习社区、在线课堂、远程教育、网络讨论、在线学习交流、同步课堂（直播）、电子书（阅读）、AR/VR教学
+商场、市场、商务中心、零售店、超市、购物中心、便利店、展销会等
+银行、证券公司、信用社、保险公司、投资公司、金融科技
+农场、田地、温室、养殖场、鱼塘、果园、牧场、谷仓等
+生产线、设备、原材料、工人、工厂、车间、生产流程、仓库
+餐厅、酒店、旅行社、美发沙龙、洗衣店、加油站、洗浴中心
+画室、工作室、艺术展览、文化节、手工市集、创意园区
+实验室、研发中心、科技园区、孵化器、测试中心、工程公司、仪器公司、软件公司、创业公司、技术转移中心（知识产权、产业化）
+电子商务、在线购物、电子支付（移动支付）、虚拟商店、共享经济、数字银行（手机银行）、金融云服务、网购平台
+电影院、影城（影视城）、电影放映、观影、音乐表演、音乐会、演唱会、音乐节、唱片店、演播室(制作）、剧院（场）、KTV，livehouse
+游乐园、动物园、植物园、水族馆、主题公园、游戏厅、桌游、麻将、棋牌、角色扮演
+酒吧、咖啡厅、茶馆、聚会、俱乐部、派对、酒会、宴会
+电子游戏、手机应用程序、在线视频、视频游戏直播、虚拟现实体验等数字娱乐形式、电子竞技、网络交友
+体育馆、健身房、体育场馆、运动场、游泳池
+公园、风景区、自然保护区、步道
+海滩、度假村、山间小屋、露营地、湖边别墅、景区、旅游区、游客服务中心、梯田、红土地、黑土地、星空营地
+古城、博物馆、古迹、文化遗产、客栈、文化村、历史建筑、古镇、古寺等
+城市街道、摩天大楼、城市夜景、商业企业、地标建筑、都市公园、步行街等都市风光。
+农家乐、田园别墅、农庄、乡村、小镇、田野、农舍、麦田水稻、牧场、山村、梯田、茶园
+人工湖泊、温泉、水库、水坝、人工河、水渠、航运水道、泄洪池等水域景观
+婚礼、生日派对、庆典、社交俱乐部、团队建设活动、聚餐、社区服务
+社交媒体、论坛、聊天室、视频聊天、网络聚会、线上派对、网络游戏、直播、语音聊天、虚拟空间
+如互联网、研究中心、电视、广播、报纸、杂志等传统媒体形式；搜索引擎、在线百科、学术数据库等信息搜索工具；
+社交网络、微博、微信等在线社交平台；网站、博客、论坛、视频分享平台等在线内容平台、网络直播、邮件组、即时通讯
+如文化公园、文化街区、民俗艺术、文化节庆、风俗节日、历史遗迹、博物馆、演出戏剧、美术馆、画廊、音乐厅、音乐会、剧院等
+教堂、寺庙、道观、清真寺、仪式、祭祀、祈祷、朝圣、礼拜、修道院、烈士陵园、陵园
+冥想中心、修行中心
+森林、雨林、针叶林、阔叶林、针阔混交林、树丛
+草甸草原、典型草原、荒漠草原、高寒草原、灌木、花海、非洲草原
+沙漠、荒漠、戈壁、沙漠绿洲、丹霞地貌、石林
+沼泽湿地、湖泊湿地、河流湿地、滨海湿地、人工湿地
+山脉、山谷、山脊、悬崖、丘陵、山峰
+北极、南极、冰山、冰盖、雪山、企鹅、北极熊
+海洋、内海、海峡、海啸、潮汐、潮
+小溪、河流、江、地下河、瀑布、长江、黄河、
+火山口湖、堰塞湖、湖、人工湖、池
+珊瑚礁、潮汐带、盐沼、海滩、沙滩、海水浴场
+河口、三角洲、淡水沼泽、芦苇荡、荷花池、滩涂
+日出、日落、流星雨、星空、彗星、日食、月食、满月
+彩虹、日月晕、雷雨、闪电、极光、朝霞晚霞、云、飓风、蜃景、雾气
+落叶、新芽、盛开、枯枝、下雪、春天、夏天、秋天、冬天
+群岛、海岛、湖心岛、岛礁
+活火山、死火山、火山喷发、富士山、火山群岛
+洞穴、石钟乳、水晶洞穴、喀斯特（岩溶地貌）
+
+
+
+
+
+
+
+#所有词汇英文翻译by claude2
+farmland, farm, plantation, orchard, woodland, hunting ground, ranch, pasture, aquafarm, fishing ground, fish pond, greenhouse, granary
+market, vegetable stand, greengrocer's, butcher's shop, supermarket, convenience store
+kitchen, bakery, food factory, pantry  
+restaurant, cafeteria, diner, fast food restaurant, bar, banquet hall, cafe, picnic area, food truck, food fair
+residence, apartment, community, villa, countryside, urban village, nursing home, household, house, bedroom, tribal settlement
+hotel, hostel, youth hostel, farm stay, holiday village, rental housing
+camper, caravan, tent, yurt, houseboat
+water utility, electricity company, gas station
+community center, plaza, gym, public green space, recreational facilities
+bus, highway/road, bridge, tunnel  
+train, high-speed rail, railway, EMU, subway, light rail, maglev train, tram
+airport, airplane
+seaport, dock, ship, speedboat, yacht, naval vessel, canoe, raft, sampan, dragon boat, bull boat
+bike path, sidewalk, pedestrian zone, parkway, fitness trail, footpath
+police, security management, city administrator, police station, police car, court, procuratorate, law firm, bank
+fire station, ambulance, air-raid shelter, refugee shelter, underpass, rescue station
+traffic light, road sign, police booth, traffic surveillance
+guard, community surveillance, access control, patrol car, security
+hospital, clinic, health center, rehab center, TCM clinic, spa, sanatorium, doctor's office, pharmacy, disease control center, vaccination site, blood donation vehicle/site, ambulance 
+school, classroom building, classroom, lecture hall, lab, seminar, study space
+playground, library, gymnasium, study room, cafe, auditorium, student activity center, club activity, campus scenery
+study, parent-monitored homework, parent-child teaching, picture book reading, outdoor exploration, housework, cooking 
+family meeting, dinner, celebration, family trip, parent-child play, birthday party, movie night, family outing
+community learning center, educational institution, community course, public library, volunteer teaching, night school, community college, elder university
+science museum, museum, art exhibition, bulletin board, bookstore, street event, volunteer activity, lecture, community meeting, social gathering, performance, festival
+online course, online tutor, learning resources, online community, online classroom, distance learning, online discussion, online exchange, live streaming, e-book, AR/VR teaching
+shopping mall, market, business district, retail store, supermarket, department store, convenience store, trade fair  
+bank, securities, credit union, insurance, investment, fintech
+farm, farmland, greenhouse, farm, fish pond, orchard, ranch, granary  
+assembly line, equipment, raw material, worker, plant, workshop, production line, warehouse
+restaurant, hotel, travel agency, hair salon, laundry, gas station, spa
+art studio, workshop, art exhibition, cultural festival, handcraft market, creative park
+lab, R&D center, tech park, incubator, testing center, engineering company, equipment company, software company, startup, tech transfer center
+e-commerce, online shopping, digital payment, virtual store, sharing economy, digital bank, financial cloud service, e-commerce platform
+movie theater, multiplex, movie screening, cinema, music performance, concert, music festival, record store, studio, theater, KTV, live house
+amusement park, zoo, botanical garden, aquarium, theme park, arcade, board game, mahjong, chess, role playing game  
+bar, cafe, teahouse, meetup, club, party, banquet, reception
+video game, phone app, online video, game live streaming, VR experience, esports, online dating
+gym, fitness center, stadium, sports field, swimming pool
+park, scenic area, nature reserve, trail  
+beach, resort, cabin, campground, lakeside villa, scenic spot, tourist area, visitor center, rice terrace, red land, black land, starry campsite
+ancient town, museum, heritage site, cultural heritage, inn, cultural village, historic architecture, ancient town, ancient temple
+city street, skyscraper, night view, enterprise, landmark, urban park, pedestrian street
+farm stay, countryside villa, farm, village, small town, rural area, cottage, wheat field, paddock, mountain village, rice paddy, tea plantation 
+reservoir, hot spring, dam, artificial river, canal, flood discharge area
+wedding, birthday party, celebration, social club, team building, dinner party, community service
+social media, forum, chat room, video chat, online party, online game, live streaming, voice chat, virtual space  
+traditional media like internet, research centers, TV, radio, newspaper, magazine; information search tools like search engine, online encyclopedia, academic database; 
+online social platforms like social network, microblog, WeChat; online content platforms like website, blog, forum, video sharing platform, online broadcast, mailing list, instant messaging
+cultural park, cultural district, folk art, festival, tradition, heritage site, museum, performance, art gallery, concert hall, music concert, theater
+church, temple, taoist temple, mosque, ritual, worship, pilgrimage, prayer, monastery, martyrs' cemetery, cemetery
+meditation center, retreat center
+forest, rainforest, coniferous forest, broadleaf forest, mixed forest, shrub 
+grassland, meadow, desert grassland, alpine grassland, scrub, flower sea, African grassland
+desert, wasteland, Gobi desert, oasis, Danxia landform, stone forest
+wetland, lake wetland, river wetland, coastal wetland, artificial wetland
+mountain range, valley, ridge, cliff, hill, peak  
+North Pole, South Pole, iceberg, ice cap, snow mountain, penguin, polar bear
+ocean, inland sea, strait, tsunami, tide
+brook, river, creek, underground river, waterfall, Yangtze River, Yellow River
+crater lake, barrier lake, lake, reservoir, pond
+coral reef, intertidal zone, salt marsh, beach, seaside beach, beach resort
+river mouth, delta, freshwater marsh, reed marsh, lotus pond, mudflat
+sunrise, sunset, meteor shower, starry sky, comet, solar eclipse, lunar eclipse, full moon
+rainbow, halo, thunderstorm, lightning, aurora, dusk and dawn glow, cloud, hurricane, mirage, fog
+falling leaves, new buds, blooming, bare branches, snowing, spring, summer, autumn, winter  
+archipelago, island, islet, reef
+active volcano, dormant volcano, volcanic eruption, Mt. Fuji, volcanic island
+cave, stalactite, crystal cave, karst landform
+
+
+
+#词汇统计
+
+#重复词汇
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
